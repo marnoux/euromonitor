d0dceb4424152bf6df5092bf3aa061c91f8bdefa
Marnoux <marnouxmanser@gmail.com>
2021-09-01 22:05:05 +0200

    add cors

--- api/Startup.cs
+++ api/Startup.cs
@@ -32,6 +32,7 @@ namespace api
         options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
       });
       services.AddControllers();
+      services.AddCors();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "api", Version = "v1" });
@@ -51,6 +52,10 @@ namespace api
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(policy => policy.AllowAnyHeader()
+      .AllowAnyMethod()
+      .WithOrigins("http://localhost:4200"));
 
       app.UseAuthorization();
 
