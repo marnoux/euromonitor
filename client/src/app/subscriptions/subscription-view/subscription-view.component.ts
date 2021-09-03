534206fc3750a96d87128a6f4f7a3735e6acf2ab
Marnoux <marnouxmanser@gmail.com>
2021-09-03 15:38:06 +0200

    add new components and theme

---
+++ client/src/app/subscriptions/subscription-view/subscription-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-view',
+  templateUrl: './subscription-view.component.html',
+  styleUrls: ['./subscription-view.component.css']
+})
+export class SubscriptionViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
